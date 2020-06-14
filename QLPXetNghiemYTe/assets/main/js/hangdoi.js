4c209f0e8210059d36154cdc50b9754f92b4287e
ntngan1298 <nganthu1298@gmail.com>
2020-06-14 10:34:42 +0700

    add benhnhancontroller

---
+++ QLPXetNghiemYTe/assets/main/js/hangdoi.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    serviceAjax.get('/', {}, true, function (res) {
+        Common.renderData('.subtable-object-header', '.subtable-object-body', res.Data, "ObjectID");
+    });
+
+
+
+});
